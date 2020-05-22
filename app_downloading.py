858014d9cc6e991acf98761cf94d812a5c52a2df
ceivv <seifeddinadel9@gmail.com>
2020-05-22 02:49:39 +0100

    initial commit

---
+++ app_downloading.py
@@ -0,0 +1,33 @@
+from selenium import webdriver 
+from bs4 import BeautifulSoup
+import requests
+ 
+
+url = 'https://m.apkpure.com/search?q='+apk
+page = requests.get(url)
+ 
+# Getting the webpage, creating a Response object.
+response = requests.get(url)
+ 
+# Extracting the source code of the page.
+data = response.text
+ 
+# Passing the source code to BeautifulSoup to create a BeautifulSoup object for it.
+soup = BeautifulSoup(data, 'lxml')
+ 
+#searching by class
+tags = soup.find_all(class_="dd")
+ 
+# Extracting URLs from the attribute href in the <a> tags.
+mylist=[]
+for tag in tags:
+   mylist.append(str(tag.get('href')))
+print(mylist)
+newurl='https://apkpure.com'+mylist[0]
+print(newurl)
+#downloading
+br=webdriver.Firefox()
+br.get(newurl)
+link=br.find_element_by_xpath('/html/body/div[3]/div[1]/div[2]/dl/dd/div[5]/a[1]')
+link.click()
+
